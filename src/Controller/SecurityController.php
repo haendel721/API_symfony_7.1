f39c14e5eae5bdc75c2e0a6e9e1b26d4f58b40fe
Haendel <abrahamhaendel8@gmail.com>
2024-09-12 12:34:17 +0300

    gerer la deconexion afin que si on ne se déconnecte pas alors il on ne peut pas se connecter

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -10,7 +12,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(Request $request, AuthenticationUtils $authenticationUtils): Response
     {
         if ($this->getUser()) {
             if( $this->getUser()->getRoles()  == 'ROLE_ADMIN'){
@@ -22,6 +24,12 @@ class SecurityController extends AbstractController
             }
             
         }
+        $session = $request->getSession();
+        if ($session->get('logged_out')) {
+            return $this->render('security/login.html.twig', [
+                'error' => 'Votre session a été invalidée. Vous devez utiliser le bouton de déconnexion pour accéder à nouveau.',
+            ]);
+        }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
@@ -30,10 +38,16 @@ class SecurityController extends AbstractController
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
+   
 
-    #[Route(path: '/logout', name: 'app_logout')]
-    public function logout(): void
+    #[Route("/logout", name:"app_logout")]
+    public function logout(Request $request): RedirectResponse
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        // Marquer la session comme nécessitant une déconnexion
+        $session = $request->getSession();
+        $session->set('logged_out', true);
+
+        // Symfony gère la déconnexion ici
+        return $this->redirectToRoute('app_homepage'); // Redirection après déconnexion
     }
 }
